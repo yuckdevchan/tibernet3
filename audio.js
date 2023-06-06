c1d795ee494922ea1059d99265481a0cd6f1dc71
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-06-06 16:14:58 +0100

    add playing and paused + make music work

--- audio.js
+++ audio.js
@@ -1,5 +1,12 @@
 var minecraft = new Audio("minecraft.mp3");
+var playing = false;
 
 function music() {
-  minecraft.play()
+  if (playing == false) {
+    document.getElementById("music_button").innerHTML = "Playing...";
+    minecraft.play();
+  } else {
+    playing = true;
+    document.getElementById("music_button").innerHTML = "Paused";
+    minecraft.pause();
 }
